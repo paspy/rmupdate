a93f2346c4cf66cf2d917f3b4051feb70e0aeede
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-04-27 05:58:17 +0000

    + 完成加密工程目录下最新文件副本的文件名加密功能

--- trunk/RMupdateManager/lib/file_encrypt.h
+++ trunk/RMupdateManager/lib/file_encrypt.h
@@ -1,10 +1,18 @@
-#ifndef
+#ifndef FILE_ENCRYPT_INCLUDE
 #define FILE_ENCRYPT_INCLUDE
 
-void* encrypt_file_content(void* file_content_buffer, long buffer_size);
-void* decrypt_file_content(void* content_buffer, long buffer_size);
+//加密文件内容
+// 加密后的内容指针 encrypt_file_content（欲加密的内容指针，欲加密的内容大小，加密后的内容大小）
+void* encrypt_file_content(void* file_content_buffer, long buffer_size, long& buffer_size_rev);
 
-char* encrypt_file_path(char* filepath);
-char* decrypt_file_path(char* filepath_chiper);
+//解密文件内容
+// 解密后的内容指针 decrypt_file_content（欲解密的内容指针，欲解密的内容大小，解密后的内容大小）
+void* decrypt_file_content(void* content_buffer, long buffer_size, long& buffer_size_rev);
+
+//加密文件路径
+char* encrypt_file_path(const char* filepath);
+
+//解密文件路径
+char* decrypt_file_path(const char* filepath_chiper);
 
 #endif
