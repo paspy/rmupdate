1ceaa31a9563f881423aeb6fc73dea78b4db2294
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-04-30 12:36:56 +0000

    * 修复了MKDIR宏的Windows部分在试图创建已存在的目录时会崩溃的鬼 * 修复了在Windows下保存并发布文件时无法发布目录下的子目录的鬼

--- RMupdateManager/RMupdateManagerApp.h
+++ RMupdateManager/RMupdateManagerApp.h
@@ -77,23 +77,22 @@ class RMupdateManagerApp : public wxApp
 //   the variable EXP should not include filename
 #if defined(__WXMSW__)
     #define MKDIR(EXP)  \
-        long __mkdir_stack[100]; \
+        long __mkdir_stack[100];    \
         long __mkdir_stack_p = 0;   \
-        char __mkdir_buf[2048]; \
-        strcpy(__mkdir_buf, EXP);   \
+        char __mkdir_buf[2048];     \
+        strcpy(__mkdir_buf, EXP);      \
         while (__mkdir_buf[++__mkdir_stack_p]) if (__mkdir_buf[__mkdir_stack_p] == '/') __mkdir_buf[__mkdir_stack_p] = '\\';  \
         __mkdir_stack_p = 0;    \
-        if (0 != _mkdir(__mkdir_buf)) { \
-            do {    \
-                __mkdir_stack[__mkdir_stack_p] = strrchr(__mkdir_buf, 92) - __mkdir_buf; \
-                __mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 0;    \
-                __mkdir_stack_p++;  \
-                if ( _mkdir(__mkdir_buf) == 0) {    \
-                    __mkdir_stack_p--;  \
-                    __mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 92; \
-                }   \
-            } while(__mkdir_stack_p != 0);  \
-        }
+		while (-1 == _access(__mkdir_buf, 0)) { \
+			__mkdir_stack[__mkdir_stack_p] = strrchr(__mkdir_buf, 92) - __mkdir_buf; \
+			__mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 0;    \
+			__mkdir_stack_p++;			\
+		}   \
+		while (--__mkdir_stack_p != -1) {   \
+            __mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 92;   \
+			_mkdir(__mkdir_buf);    \
+		}   \
+		_mkdir(__mkdir_buf);
 #elif defined(__UNIX__)
     #define MKDIR(EXP)  \
         char __mkdir_cmd[2048]; \
