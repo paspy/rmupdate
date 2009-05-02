e9ffa45ddd94aa2b141092a187c3c2caa7aa90d1
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-05-02 15:59:26 +0000

    + 初步完成下载更新信息文件的部分

--- trunk/RMupdatER/RMupdaterMain.cpp
+++ trunk/RMupdatER/RMupdaterMain.cpp
@@ -20,6 +20,7 @@
 #include "RMupdaterConfig.h"
 
 #include <curl/curl.h>
+#include "ticpp/tinyxml.h"
 
 //helper functions
 enum wxbuildinfoformat {
@@ -32,7 +33,6 @@ RMupdaterFrame::RMupdaterFrame(wxFrame *frame, const wxString& title)
     : FrameUpdater(frame, -1, title)
 {
     m_buttonUpdate->Enable(false);
-
 }
 
 
@@ -65,7 +65,6 @@ void RMupdaterFrame::OnConfig(wxCommandEvent& event)
 
 void RMupdaterFrame::OnCheck(wxCommandEvent& event)
 {
-    SetStatus(_T("checking updates..."));
     m_buttonCheck->Enable(false);
     CheckNewest();
 }
@@ -90,9 +89,61 @@ void RMupdaterFrame::CheckNewest()
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &RMupdaterFrame::curl_writefunction_check);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &curl_in);
     curl_easy_perform(curl);
+
+    long http_code;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
     curl_easy_cleanup(curl);
 
-    //
+    if (http_code != 200 && http_code != 206) {
+    	wxString info;
+    	info.Printf(_T("下载更新文件发生错误，HTTP错误代码：%ld"), http_code);
+    	wxMessageDialog(NULL, info, _T("错误"), wxOK | wxICON_EXCLAMATION).ShowModal();
+    	m_buttonCheck->Enable(true);
+    	SetStatus(info);
+    	return;
+    }
+    else {
+    	TiXmlDocument doc;
+		long absver, subabsver, updatetime;
+		wxString version;
+
+    	if (!doc.Parse((const char*)curl_in.buffer)) {
+    		wxString info;
+    		wxString errinfo;
+    		errinfo = wxString(doc.ErrorDesc(), wxConvLibc);
+    		info.Printf(_T("加载更新文件时发生错误：") + errinfo );
+    		wxMessageDialog(NULL, info, _T("错误"), wxOK | wxICON_EXCLAMATION).ShowModal();
+    		m_buttonCheck->Enable(true);
+    		SetStatus(info);
+    		return;
+    	}
+    	else {
+    		TiXmlHandle hDoc(&doc);
+    		TiXmlHandle root = hDoc.ChildElement("update", 0);
+
+    		char tmp[1024];
+    		const char* val;
+
+			strcpy(tmp, root.ChildElement("version", 0).ToElement()->GetText());
+			version = wxString(tmp, wxConvLibc);
+			val = root.ChildElement("AbsVer", 0).ToElement()->GetText();
+			absver = val ? atol(val) : 0;
+			val = root.ChildElement("SubAbsVer", 0).ToElement()->GetText();
+			subabsver = val ? atol(val) : 0;
+			val = root.ChildElement("UpdateTime", 0).ToElement()->GetText();
+			updatetime = val ? atol(val) : 0;
+    	}
+
+    	//检查更新情况
+    	if (config.AbsVer != absver || config.SubAbsVer != subabsver) {
+    		//有更新
+    		m_buttonUpdate->Enable(true);
+    	}
+    	else {
+    		//没有更新
+    	}
+
+	}
 
 
 }
@@ -109,6 +160,12 @@ size_t RMupdaterFrame::curl_writefunction_check(void *ptr, size_t size, size_t n
     double content_length;
     curl_easy_getinfo(in->curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &content_length);
 
+    //检查HTTP代码
+    double http_code;
+    curl_easy_getinfo(in->curl, CURLINFO_RESPONSE_CODE, &http_code);
+    //if (http_code != 200 && http_code != 206) return 0;
+
+	//初始化缓冲区
     if (in->buffer == NULL) {
     	in->buffer = malloc(content_length + 1);
     	in->buffer_ptr = 0;
@@ -118,9 +175,9 @@ size_t RMupdaterFrame::curl_writefunction_check(void *ptr, size_t size, size_t n
     memcpy((char*)in->buffer + in->buffer_ptr, ptr, read_size);
     in->buffer_ptr += read_size;
 
-    char info[1024];
-    sprintf(info, "downloading update info... %ld bytes in %0.0lf bytes", in->buffer_ptr, content_length);
-    pFrameUpdater->SetStatus(wxString(info, wxConvLibc));
+    wxString info;
+    info.Printf(_T("正在下载更新文件，共 %1$ld 字节，已经下载了 %2$ld 字节"), content_length, in->buffer_ptr);
+    pFrameUpdater->SetStatus(info);
     pFrameUpdater->m_gaugeCurrent->SetValue(in->buffer_ptr * 100 / content_length);
     pFrameUpdater->m_gaugeTotal->SetValue(in->buffer_ptr * 100 / content_length);
     pFrameUpdater->m_gaugeCurrent->Update();
