ec1392c0e2635fa0a574fb3f82a4d253e28cabeb
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-05-13 11:16:13 +0000

    + 完成到下载单个更新文件部分（不包括CURL回调函数）

--- trunk/RMupdatER/RMupdaterApp.h
+++ trunk/RMupdatER/RMupdaterApp.h
@@ -50,4 +50,35 @@ class RMupdaterApp : public wxApp
 
 #define pFrameUpdater ((RMupdaterFrame*)wxGetApp().frame)
 
+
+// define MKDIR(EXP)
+//  this cross-platform macro can make directory recuresively
+//   the variable EXP should not include filename
+#if defined(__WXMSW__)
+    #define MKDIR(EXP)  \
+        long __mkdir_stack[100];    \
+        long __mkdir_stack_p = 0;   \
+        char __mkdir_buf[2048];     \
+        strcpy(__mkdir_buf, EXP);      \
+        while (__mkdir_buf[++__mkdir_stack_p]) if (__mkdir_buf[__mkdir_stack_p] == '/') __mkdir_buf[__mkdir_stack_p] = '\\';  \
+        __mkdir_stack_p = 0;    \
+		while (-1 == _access(__mkdir_buf, 0)) { \
+			__mkdir_stack[__mkdir_stack_p] = strrchr(__mkdir_buf, 92) - __mkdir_buf; \
+			__mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 0;    \
+			__mkdir_stack_p++;			\
+		}   \
+		while (--__mkdir_stack_p != -1) {   \
+            __mkdir_buf[__mkdir_stack[__mkdir_stack_p]] = 92;   \
+			_mkdir(__mkdir_buf);    \
+		}   \
+		_mkdir(__mkdir_buf);
+#elif defined(__UNIX__)
+    #define MKDIR(EXP)  \
+        char __mkdir_cmd[2048]; \
+        sprintf(__mkdir_cmd, "mkdir -p \"%s\"", EXP);   \
+        system(__mkdir_cmd);
+#endif
+// end define MKDIR(EXP)
+
+
 #endif // RMUPDATERAPP_H
