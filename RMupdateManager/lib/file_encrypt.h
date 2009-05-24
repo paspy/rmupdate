beeccedad7a7cf0354f4f68a850ef4bb1e70a2ba
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-05-24 03:48:12 +0000

    * 按照工程规范，改变管理程序在非加密编译时输出的资源文件文件名

--- RMupdateManager/lib/file_encrypt.h
+++ RMupdateManager/lib/file_encrypt.h
@@ -1,6 +1,7 @@
 #ifndef FILE_ENCRYPT_INCLUDE
 #define FILE_ENCRYPT_INCLUDE
 
+#ifdef RMUPDATE_ENCRYPT_FILE
 //加密文件内容
 // 加密后的内容指针 encrypt_file_content（欲加密的内容指针，欲加密的内容大小，加密后的内容大小）
 void* encrypt_file_content(void* file_content_buffer, long buffer_size, long& buffer_size_rev);
@@ -9,10 +10,12 @@ void* encrypt_file_content(void* file_content_buffer, long buffer_size, long& bu
 // 解密后的内容指针 decrypt_file_content（欲解密的内容指针，欲解密的内容大小，解密后的内容大小）
 void* decrypt_file_content(void* content_buffer, long buffer_size, long& buffer_size_rev);
 
-//加密文件路径
-char* encrypt_file_path(const char* filepath);
-
 //解密文件路径
 char* decrypt_file_path(const char* filepath_chiper);
 
-#endif
+#endif // RMUPDATE_ENCRYPT_FILE
+
+//加密文件路径
+char* encrypt_file_path(const char* filepath);
+
+#endif //FILE_ENCRYPT_INCLUDE
