12fb2e3cb4f754e77c7ef839df58e60379f1bde9
gs@bbxy.net <gs@bbxy.net@6ea0c044-2fc3-11de-8816-6191455234fe>
2009-06-07 08:16:25 +0000

    * 试图修正更新程序在非第一次更新时写入的资源包文件中只有更新过的文件的鬼（issue5）

--- RMupdatER/RMupdaterMain.h
+++ RMupdatER/RMupdaterMain.h
@@ -64,6 +64,7 @@ class RMupdaterFrame: public FrameUpdater
         void SetCurProcLabel(const wxString& info);
         void SetTtlProcLabel(const wxString& info);
         static wxString HumanReadSize(double speed_bytes);
+        void ApplyNotUpdateRgss2a(file_list_t& list);
 };
 
 
